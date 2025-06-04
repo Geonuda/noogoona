3d404d61983d6fcbc5d55651cccafbd2cd03de50
Geonuda <dn9685@naver.com>
2025-06-04 14:51:46 +0900

    Remove comments from app.py and fix image permissions

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
 from flask import Flask, render_template
 import os
 
-# tamplat 폴더를 템플릿 폴더로 지정
 app = Flask(__name__, template_folder='tamplat', static_folder='static')
 
 @app.route('/')
 def home():
-    # 개인 정보 데이터
     profile_data = {
         'name': '김건우',
         'age': '25세',
@@ -17,6 +15,5 @@ def home():
     return render_template('index.html', profile=profile_data)
 
 if __name__ == '__main__':
-    # Render에서는 PORT 환경변수를 사용
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=False)
